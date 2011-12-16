e1f7fe6699f6bc358b758b188e3c2dd487236bf7
Sergio Rubio <rubiojr@frameos.org>
2011-12-16 17:52:55 +0100

    * added 2.0 tests

---
+++ 2.0/ciab_fixed_ip/config.rb
@@ -0,0 +1,8 @@
+ScriptConfig.create do 
+  root_password 'abiquo'
+  server_ip '10.60.20.49'
+  netmask '24'
+  hostname 'abiquo-ciab'
+  gateway '10.60.20.1'
+  dns_server '10.60.1.4'
+end
---
+++ 2.0/vbox-cn/config.rb
@@ -0,0 +1,10 @@
+ScriptConfig.create do 
+  root_password 'abiquo'
+  server_ip '10.60.20.57'
+  netmask '24'
+  hostname 'abiquo-vbox'
+  gateway '10.60.20.1'
+  dns_server '10.60.1.4'
+  nfs_server '10.60.20.49'
+  remote_services '10.60.20.49'
+end
