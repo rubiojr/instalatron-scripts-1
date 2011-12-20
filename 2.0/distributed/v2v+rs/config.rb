05bcc04458e4ba954f1966f0176f1fe9c577a238
Sergio Rubio <rubiojr@frameos.org>
2011-12-20 15:20:00 +0100

    * fixes and a couple of new test scripts

--- 2.0/distributed/v2v+rs/config.rb
+++ 2.0/distributed/v2v+rs/config.rb
@@ -1,4 +1,6 @@
 ScriptConfig.create do 
-  root_password 'abiquo'
+  server_ip '10.0.0.1'
+  nfs_url '10.0.0.1:/opt/vm_repository'
+  datacenter_id 'Abiquo'
   bootloader_args 'linux updates=http://mirror.bcn.abiquo.com/updates-dev.img<enter>'
 end
---
+++ 2.0/distributed/v2v/config.rb
@@ -0,0 +1,5 @@
+ScriptConfig.create do 
+  server_ip '10.0.0.1'
+  nfs_url '10.0.0.1:/opt/vm_repository'
+  datacenter_id 'Abiquo'
+end
