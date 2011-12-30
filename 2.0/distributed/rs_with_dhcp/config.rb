3fd252e0d61f5083d2d62d67c5de407e2a0ef470
Sergio Rubio <rubiojr@frameos.org>
2011-12-30 13:17:35 +0100

    * Added new tests

---
+++ 2.0/distributed/rs_with_dhcp/config.rb
@@ -0,0 +1,6 @@
+ScriptConfig.create do 
+  server_ip '10.0.0.1'
+  nfs_url '10.0.0.1:/opt/vm_repository'
+  datacenter_id 'Abiquo'
+  bootloader_args 'linux http://mirror.bcn.abiquo.com/updates-dev.img<enter>'
+end
