d2aba03b8980774d15beaf1a74d64c3c25fb792d
Sergio Rubio <rubiojr@frameos.org>
2012-01-27 12:02:00 +0100

    new script monolithic+dhcp

---
+++ 2.0/monolithic+dhcp/config.rb
@@ -0,0 +1,9 @@
+ScriptConfig.create do 
+  root_password 'abiquo'
+  server_ip '10.60.20.49'
+  netmask '24'
+  hostname 'abiquo-mono'
+  gateway '10.60.20.1'
+  dns_server '10.60.1.4'
+  nfs_repo '10.60.1.225'
+end
