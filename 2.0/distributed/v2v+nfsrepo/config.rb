6e33f85e823757f6d10390f76edcb40c748b7987
Sergio Rubio <rubiojr@frameos.org>
2012-01-31 10:55:07 +0100

    * updated v2v+nfsrepo script

--- 2.0/distributed/v2v+nfsrepo/config.rb
+++ 2.0/distributed/v2v+nfsrepo/config.rb
@@ -1,5 +1,9 @@
 ScriptConfig.create do 
-  server_ip '10.0.0.1'
-  nfs_url '10.0.0.1:/opt/vm_repository'
+  abiquo_server_ip '10.60.1.40'
+  host_ip '10.60.1.43'
+  host_mask '24'
+  host_gateway '10.60.1.4'
+  host_nameserver '10.60.1.4'
   datacenter_id 'Abiquo'
+  bootloader_args "#{ENV[ABI_BOOTLOADER_ARGS]}"
 end
